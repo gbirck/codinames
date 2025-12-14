a5920f1d5ee944bcc0a5d22cc27495364739882a
Gabriel Birck <birckzin@gmail.com>
2025-12-14 03:16:55 -0300

    Iniciando projeto de desafio de herois/codinome

---
+++ src/main/java/com/gabrielbirck/codinames/CodinamesApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielbirck.codinames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodinamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodinamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabrielbirck/codinames/CodinamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielbirck.codinames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodinamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
